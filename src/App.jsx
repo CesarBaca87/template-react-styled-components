ade0e0d290eaa8d2e538d0db48ce7ab212be6c99
BacaCesar <cesarbaca.fotografia@gmail.com>
2024-10-08 23:35:24 +0200

    template update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Button from './components/button/Button';
+import { GlobalStyles } from './styles/GlobalStyles';
+
 const App = () => {
   return (
     <>
+      <GlobalStyles />
       <h1>Curso de React</h1>
+      <Button color='steelblue'>Click Me!</Button>
+      <Button color='#f9f'>Click Me!</Button>
     </>
   );
 };
---
+++ src/components/button/button.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    background-color: ${({color} )=>color};
+    color: white;
+    border: none;
+    padding: 0.5rem;
+
+    @media screen and (width>768px){
+        display: block;
+    }
+
+    @media (hover:hover){
+        &:hover{
+        scale:0.6;
+    }
+}
+`
+export {StyledButton}
